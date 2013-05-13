0a9b83acd301035361081dd47942504d29bdc894
Itai Birenshtok <itai.biren@gmail.com>
2013-05-13 17:40:22 +0300

    windows azure integration at parent's house

---
+++ PHPPage.php
@@ -0,0 +1,14 @@
+<?php
+    mail('itai.biren@gmail.com','hi','hi', 'From: itai.biren@gmail.com');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
