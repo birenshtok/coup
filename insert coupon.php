d7e1d014f6dece4d12bb10a83cdbc64368be3854
Itai Birenshtok <itai.biren@gmail.com>
2013-05-14 12:45:36 +0300

    add the discount, and change the price_pattern to a better one.

--- insert coupon.php
+++ insert coupon.php
@@ -1,16 +1,24 @@
 <?php
     function insert_coup($url,$category){
         print($url);
-         $handle = fopen("$url", "r");
-         //$write_handle = fopen("C:\Itai\\coupon-test.txt", "w");
+        $handle = fopen("$url", "r");
+        //$write_handle = fopen("C:\Itai\\coupon-test.txt", "w");
         while (!feof($handle)){
-         $text = fgets($handle);
-         $pattern = '/"noWrap"(.*?)₪<\/span/';
-         if(preg_match($pattern, $text, $matches)){
-            $price = $matches[1];
-            print ($price);
-            break;
-         }
+            $text = fgets($handle);
+            $discount_pattern = '/"savings2_values">([\\d]*)/';
+            $price_pattern = '/"noWrap">([\\d,]*)/'; //the pattern to find the price take in calc that the price may be more than 3 digits.
+            if((preg_match($price_pattern, $text, $matches)) && (!$price_flag)){
+                $price = $matches[1];
+                print ("<BR> Price: ");
+                print ($price);
+                $price_flag = 1;
+            }
+            if((preg_match($discount_pattern, $text, $matches)) && (!$discount_flag)){
+                $discount_pattern = $matches[1];
+                print ("<BR> discount: ");
+                print ($discount_pattern);
+                $discount_flag = 1;
+            }
         }
     }
      
