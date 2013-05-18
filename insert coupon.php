5cb9e6bda852023a13388e3b0aee57234b1b2197
Itai Birenshtok <itai.biren@gmail.com>
2013-05-18 22:19:34 +0300

    Add place and category

--- insert coupon.php
+++ insert coupon.php
@@ -2,24 +2,34 @@
     function insert_coup($url,$category){
         print($url);
         $handle = fopen("$url", "r");
-        //$write_handle = fopen("C:\Itai\\coupon-test.txt", "w");
         while (!feof($handle)){
             $text = fgets($handle);
             $discount_pattern = '/"savings2_values">([\\d]*)/';
             $price_pattern = '/"noWrap">([\\d,]*)/'; //the pattern to find the price take in calc that the price may be more than 3 digits.
-            if((preg_match($price_pattern, $text, $matches)) && (!$price_flag)){
-                $price = $matches[1];
-                print ("<BR> Price: ");
-                print ($price);
-                $price_flag = 1;
+            $place_pattern = '/placename" content="(.*?)"/';
+            if (!$price_flag) {
+                if(preg_match($price_pattern, $text, $matches)) {
+                    $price = $matches[1];
+                    print ("<BR> Place: $price");
+                    $price_flag = 1;
+                }
             }
-            if((preg_match($discount_pattern, $text, $matches)) && (!$discount_flag)){
-                $discount_pattern = $matches[1];
-                print ("<BR> discount: ");
-                print ($discount_pattern);
-                $discount_flag = 1;
+            if (!$discount_flag) {
+                if(preg_match($discount_pattern, $text, $matches)) {
+                    $discount = $matches[1];
+                    print ("<BR> Place: $discount");
+                    $discount_flag = 1;
+                }
+            }
+            if (!$place_flag) {
+                if(preg_match($place_pattern, $text, $matches)) {
+                    $place = $matches[1];
+                    print ("<BR> Place: $place");
+                    $place_flag = 1;
+                }
             }
         }
+        print ("<BR> Category: $category");
     }
      
 ?>
--- open url.php
+++ open url.php
@@ -5,11 +5,15 @@
     $i = 0;
     while (!feof($handle)) {
         $text = fgets($handle);
-        $pattern = '/"dealPermaLink":"(.*?)"/';
-        preg_match_all($pattern, $text, $matches);
-        foreach ($matches[1] as $Link) {
-            insert_coup ("http://www.groupon.co.il$Link","df");
+        $pattern_Link = '/"dealPermaLink":"(.*?)"/';
+        preg_match_all($pattern_Link, $text, $matches_Link);
+        $pattern_Category = '/itemsLocalDeals = \[{"category":"(.*?)"/';
+        preg_match_all($pattern_Category, $text, $matches_Category);
+        foreach ($matches_Link[1] as $Link) {
+            $category = $matches_Category[1][$i];
+            insert_coup ("http://www.groupon.co.il$Link",$category);
             print ("<BR>");
+            ++$i;
             break; // Just foe testing a single coupon.
         }
     }
