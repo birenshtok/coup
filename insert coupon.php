c4ce5a5ad990cbe238fcddaccc25b456c38cda0c
shye <shye.biren@gmail.com>
2013-07-01 01:15:34 +0300

    Revert "finish to organize the coupon name by the keywords."
    This reverts commit 2e585a341a57ee260d3c6621eb894872d3854d72.

--- insert coupon.php
+++ insert coupon.php
@@ -7,7 +7,6 @@
             $discount_pattern = '/"savings2_values">([\\d]*)/';
             $price_pattern = '/"noWrap">([\\d,]*)/'; //the pattern to find the price take in calc that the price may be more than 3 digits.
             $place_pattern = '/placename" content="(.*?)"/';
-            $name_pattern = '/<meta name="keywords" content="(.*?)"/';
             if (!$price_flag) {
                 if(preg_match($price_pattern, $text, $matches)) {
                     $price = $matches[1];
@@ -29,27 +28,18 @@
                     $place_flag = 1;
                 }
             }
-            if (!$name_flag) {
-                if(preg_match($name_pattern, $text, $matches)) {
-                    $name = $matches[1];
-                    print ("<BR> Name: $name");
-                    $name_flag = 1;
-                }
-            }
-
-            if ($price_flag && $discount_flag && $place_flag && $name_flag) // for efficiency
-                break;
         }
         print ("<BR> Category: $category");
         fclose($handle);
-    }    
+    }
+     
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title> Finding the attributes</title>
+        <title></title>
     </head>
     <body>
         
--- open url.php
+++ open url.php
@@ -3,7 +3,6 @@
     include "insert coupon.php";
     $handle = fopen("http://www.groupon.co.il/all-deals/tel-aviv-iw", "r");
     $i = 0;
-    $j = 0;
     while (!feof($handle)) {
         $text = fgets($handle);
         $pattern_Link = '/"dealPermaLink":"(.*?)"/';
@@ -12,18 +11,10 @@
         preg_match_all($pattern_Category, $text, $matches_Category);
         foreach ($matches_Link[1] as $Link) {
             $category = $matches_Category[1][$i];
+            insert_coup ("http://www.groupon.co.il$Link",$category);
+            print ("<BR>");
             ++$i;
-
-            /* do only if this is a restaurant coupon */
-            if ($category == "RESTAURANT1") {
-                $j++;
-                print("$j. ");
-                insert_coup ("http://www.groupon.co.il$Link",$category);
-                
-                print ("<BR><BR>"); // a line between coupons
-                
-                /*break; // Just for testing a single coupon.*/
-            }
+            break; // Just for testing a single coupon.
         }
     }
     fclose($handle);
