2f5aa74408b1909fb653d74634f1ac223a57f2c3
shye <shye.biren@gmail.com>
2013-08-25 16:36:26 +0300

    add Documentation

--- template.php
+++ template.php
@@ -1,5 +1,13 @@
+<!--
+   25/08/13 this is the class that will get the patterns, from the templete text.
+            if you want to get the first site then call the constrctor with 0,
+            second site 1, sered site 2...
+   **shye 1 
+--!>
+    
 <?php
     
+
 class template{
         private $SiteName;
         private $pattern_Link;
